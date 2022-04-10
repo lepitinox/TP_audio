d46ecc9dc1292ed7a58946f10963d878906b8e61
Adrien <adrienducourthial@gmail.com>
2022-04-10 22:13:31 +0200

    base notebook

--- TP4/files/Cod_inv.py
+++ TP4/files/Cod_inv.py
@@ -8,12 +8,13 @@ Created on Fri Apr  2 11:10:49 2021
 """
 import numpy as np
 
-def Fuquant_inv(xq,R):
-    if R==0:
+
+def fuquant_inv(xq, r):
+    if r == 0:
         return 0
-    else :
-        yq=np.round(xq)
-        sign=-2*(yq//(2**(R-1)))+1
-        Q=2/(2**R-1)
-        x=yq%(2**(R-1))
-        return sign*Q*x
+    else:
+        yq = np.round(xq)
+        sign = -2 * (yq // (2 ** (r - 1))) + 1
+        q = 2 / (2 ** r - 1)
+        x = yq % (2 ** (r - 1))
+        return sign * q * x
--- TP4/files/QuantCod.py
+++ TP4/files/QuantCod.py
@@ -7,18 +7,19 @@ Created on Fri Apr  2 11:10:49 2021
 """
 import numpy as np
 
-def Fuquant(x, N):
-# quantificateur uniforme sur n bits
-# saturation  +-1
-# entrée: vecteur de reels
-# sortie: vecteur d'entiers positifs
 
-    if N>0:
-        pown = 2**(N-1)
-        xq = pown*(x<0) + np.floor( pown *np.min([np.abs(x),1-1e-10]) )
+def fuquant(x, n):
+    """
+    quantificateur uniforme sur n bits
+    saturation  +-1
+    entrée: vecteur de reels
+    sortie: vecteur d'entiers positifs"""
+
+    if n > 0:
+        pown = 2 ** (n - 1)
+        xq = pown * (x < 0) + np.floor(pown * np.min([np.abs(x), 1 - 1e-10]))
     else:
         # xq = []
         xq = 0
-        
 
     return xq
